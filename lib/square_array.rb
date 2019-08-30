0da343fa3582ae9fc23dd06656a5c6858d7bd3ed
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-08-30 15:58:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(square_array)
  counter = 0 
  while counter < square_array.length do 
    puts square_array[counter]**2
+   new_array << (square_array[counter]**2)
    counter +=1
-   new_array.push(square_array[counter]**2)
 end
 end
 
